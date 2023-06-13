ac7496e52b03de4b250724c1d602af0a830a9f79
OUSSAMA OULOUARK <alioulouark@gmail.com>
2023-06-13 01:14:26 +0100

    exe01 complete

--- Super mini/app/src/main/java/com/example/myapplication/miniproject.java
+++ Super mini/app/src/main/java/com/example/myapplication/miniproject.java
@@ -1,18 +1,25 @@
 package com.example.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
+import android.widget.RadioButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 
 public class miniproject extends AppCompatActivity implements View.OnClickListener {
 
     Button btnusersactloadusers , btnuseractquit ;
+    RadioButton Rdmale ,Rdfemale;
 
 
     @Override
@@ -22,6 +29,8 @@ public class miniproject extends AppCompatActivity implements View.OnClickListen
 
         btnusersactloadusers = findViewById(R.id.btnusersactloadusers);
         btnuseractquit = findViewById(R.id.btnuseractquit);
+        Rdfemale = findViewById(R.id.Rbfemale);
+        Rdmale = findViewById(R.id.Rbmale);
 
         btnusersactloadusers.setOnClickListener(this);
         btnuseractquit.setOnClickListener(this);
@@ -32,27 +41,54 @@ public class miniproject extends AppCompatActivity implements View.OnClickListen
 
     @Override
     public void onClick(View v) {
-        if (v.getId() == R.id.btnusersactloadusers){
+        if (v.getId() == R.id.btnusersactloadusers) {
             try {
                 InputStream inputStream = getAssets().open("users.json");
-                int code ;
+                int code;
                 StringBuilder stringBuilder = new StringBuilder();
-                String jsonstring ;
-                code = inputStream.read();
-                while (code != -1){
+                String jsonString;
+
+                while ((code = inputStream.read()) != -1) {
                     stringBuilder.append((char) code);
-                    code = inputStream.read();
                 }
-                jsonstring = stringBuilder.toString();
-                Toast.makeText(this, jsonstring, Toast.LENGTH_SHORT).show();
 
+                jsonString = stringBuilder.toString();
+                JSONObject jsonObject = new JSONObject(jsonString);
+                JSONArray jsonArray = jsonObject.getJSONArray("users");
 
-            }catch (IOException e) {
+                StringBuilder stringBuilderFullname = new StringBuilder();
+                String title = null;
+
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    JSONObject user = jsonArray.getJSONObject(i);
+                    String gender = user.getString("gender");
+
+                    if ((gender.equals("male") && Rdmale.isChecked())  ||
+                            (gender.equals("female") && Rdfemale.isChecked())) {
+
+                        JSONObject username = user.getJSONObject("name");
+                        String fullName = String.format("%s %s | %s\n",
+                                username.getString("first"),
+                                username.getString("last"),
+                                user.getString("city"));
+                        stringBuilderFullname.append(fullName);
+                        title = (gender.equals("male")) ? "Male Users" : "Female Users";
+                    }
+                }
+
+                AlertDialog.Builder alertDialog = new AlertDialog.Builder(miniproject.this);
+                alertDialog.setTitle(title);
+                alertDialog.setMessage(stringBuilderFullname.toString());
+                alertDialog.show();
+
+            } catch (IOException | JSONException e) {
                 e.printStackTrace();
             }
-        }else if (v.getId() == R.id.btnuseractquit){
-
+        } else if (v.getId() == R.id.btnuseractquit) {
             finish();
         }
     }
+
 }
+
+
