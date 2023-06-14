b8f64b2f626f23a0474e631caec3eef08264ad41
OUSSAMA OULOUARK <alioulouark@gmail.com>
2023-06-14 02:30:48 +0100

    Display user in costum view list view add details button

--- Super mini/app/src/main/java/com/example/myapplication/usersadapter.java
+++ Super mini/app/src/main/java/com/example/myapplication/usersadapter.java
@@ -7,16 +7,21 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class usersadapter extends BaseAdapter {
+    Context context;
+
     ArrayList<User> users;
+
     LayoutInflater inflater;
 
     public usersadapter(Context context, ArrayList<User> users) {
         this.users = users;
+        this.context = context;
 
         inflater = LayoutInflater.from(context);
     }
@@ -43,11 +48,13 @@ public class usersadapter extends BaseAdapter {
 
         TextView tvuserfullname = convertView.findViewById(R.id.TvuserItemFullname);
         TextView tvusercity = convertView.findViewById(R.id.Tviusercity);
-        TextView tvusergender = convertView.findViewById(R.id.Tvusergender);
+        Button btndetailsitem = convertView.findViewById(R.id.btndetailsitem);
 
         tvuserfullname.setText(users.get(position).fullName());
         tvusercity.setText(users.get(position).getCity());
-        tvusergender.setText(users.get(position).getGender());
+        btndetailsitem.setOnClickListener(v ->{
+
+        });
 
 
         return convertView;
