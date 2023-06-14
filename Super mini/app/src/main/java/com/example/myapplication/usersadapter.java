da25c753fa72897eaa9b90d687c960f161a939cf
OUSSAMA OULOUARK <alioulouark@gmail.com>
2023-06-14 10:43:26 +0100

    setintouchlistnner to user item

--- Super mini/app/src/main/java/com/example/myapplication/usersadapter.java
+++ Super mini/app/src/main/java/com/example/myapplication/usersadapter.java
@@ -3,10 +3,12 @@ package com.example.myapplication;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AlertDialog;
 
@@ -64,6 +66,17 @@ public class usersadapter extends BaseAdapter {
                return false;
            }
        });
+
+       convertView.setOnTouchListener(new View.OnTouchListener() {
+           @Override
+           public boolean onTouch(View v, MotionEvent event) {
+               if (event.getAction() == MotionEvent.ACTION_DOWN)
+
+                Toast.makeText(context, "down", Toast.LENGTH_SHORT).show();
+               return true;
+           }
+       });
+
 
 
 
